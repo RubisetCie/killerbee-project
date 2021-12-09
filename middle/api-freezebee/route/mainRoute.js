714e1e2af6f036389d0743bb83f14804de8baece
Matthieu Carteron <rubisetcie@gmail.com>
2021-12-09 10:10:58 +0100

    Edited package.json

--- middle/api-freezebee/route/mainRoute.js
+++ middle/api-freezebee/route/mainRoute.js
@@ -1,9 +1,9 @@
 /****************************************************************
- * The routes to perform the main operations.
+ * The routes to perform the model operations.
  ****************************************************************/
 
 const router = require("express").Router();
-const controller = require("../controller/mainController");
+const controller = require("../controller/modelController");
 
 router.get("/", controller.getAll);
 router.get("/restaurant/:id", controller.getByRestaurantId);

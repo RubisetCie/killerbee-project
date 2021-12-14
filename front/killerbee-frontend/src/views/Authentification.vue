61e04adc5b95ddc52c778866e59b6e75750710fe
JOHNS <luna.johns@outlook.fr>
2021-12-14 09:23:08 +0100

    v9 Killerbee-frontend (avec JS)

--- front/killerbee-frontend/src/views/Authentification.vue
+++ front/killerbee-frontend/src/views/Authentification.vue
@@ -9,27 +9,26 @@
                 <h2 style="text-decoration: cursive">Killerbee</h2>
             </v-app-bar>
         </div>
-        <div id="ContentAuthentification" class="col-md-12">
+        <div id="ContentAuthentification">
             <div class="card card-container">
                 <img id="profile-img" :src="srcPicture" class="profile-img-card"/>
-                <v-col cols="4" class="zone">
-                    <h2 class="title">LOGIN</h2>
-                    <validation-observer ref="observer">
-                        <v-form @submit.prevent="submit">
-                            <validation-provider  v-slot="{ errors }" name="Email" rules="required|email">
-                                <v-text-field v-model="email" class="zone" :error-messages="errors" label="Email" required outlined dark filled dense></v-text-field>
-                            </validation-provider>
-                            <validation-provider color="#087A57" v-slot="{ errors }" name="Password" rules="required">
-                                <v-text-field v-model="password" class="zone" :error-messages="errors" label="Password" :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'" @click:append="showPass = !showPass" required outlined dense dark filled :type="showPass ? 'text' : 'password'"></v-text-field>
-                            </validation-provider>
-                            <div class="text-center">
-                                <v-btn class="signin-btn" type="submit" rounded color="#087A57" dark>
-                                    Sign In
-                                </v-btn>
-                            </div>
-                        </v-form>
-                    </validation-observer>
-                </v-col>
+                <v-divider class="mx-4"></v-divider>
+                <h2 class="title">LOGIN</h2>
+                <validation-observer ref="observer">
+                    <v-form @submit.prevent="submit">
+                        <validation-provider  v-slot="{ errors }" name="Email" rules="required|email">
+                            <v-text-field v-model="email" class="zone" :error-messages="errors" label="Email" required outlined dark filled dense></v-text-field>
+                        </validation-provider>
+                        <validation-provider color="#087A57" v-slot="{ errors }" name="Password" rules="required">
+                            <v-text-field v-model="password" class="zone" :error-messages="errors" label="Password" :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'" @click:append="showPass = !showPass" required outlined dense dark filled :type="showPass ? 'text' : 'password'"></v-text-field>
+                        </validation-provider>
+                        <div class="text-center">
+                            <v-btn class="signin-btn" type="submit" rounded color="#087A57" dark>
+                                Sign In
+                            </v-btn>
+                        </div>
+                    </v-form>
+                </validation-observer>
             </div>
         </div>  
     </v-container>
